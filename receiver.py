37edf87e5989a1e37874292a8b402371816f0039
Henrique de Lima Xavier <henrique.xavier@cwi.com.br>
2023-05-20 03:15:53 -0300

    send and receive files

--- receiver.py
+++ receiver.py
@@ -1,15 +1,40 @@
 import socket
+from bytes import *
+from math import *
+from consts import *
 
-IP = '127.0.0.1'
-PORT = 34754
-FORMAT = 'utf-8'
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket.bind((IP, PORT))
 
+
+def receive_file():
+    file_size, address = socket.recvfrom(1024)
+
+    file_size = int(file_size.decode(FORMAT))
+
+    blocks = int(ceil(file_size / BLOCK_SIZE))
+    padding = file_size % BLOCK_SIZE
+
+    with open('new_file.txt', 'wb') as file:
+        for i in range(blocks):
+            data, address = socket.recvfrom(BLOCK_SIZE)
+
+            if i == blocks - 1:
+                data = unpad(data, padding)
+
+            file.write(data)
+
+
+print('Waiting for messages...')
 while True:
+
     message, address = socket.recvfrom(1024)
     message = message.decode(FORMAT)
 
-    print(f'{address}: {message}')
+    if message == SEND_FILE:
+        receive_file()
+        print(f'File received from {address}')
+    else:
+        print(f'{address}: {message}')
     
--- sender.py
+++ sender.py
@@ -1,15 +1,37 @@
 import socket
+from bytes import *
+from os import path
+from math import ceil
+from consts import *
 
-IP = '127.0.0.1'
-PORT = 34754
-FORMAT = 'utf-8'
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 
 def send(message):
     message = message.encode(FORMAT)
     socket.sendto(message, (IP, PORT))
 
+
+def send_file(file_name):    
+    file_size = path.getsize('file.txt')
+    blocks = int(ceil(file_size / BLOCK_SIZE))
+
+    send(str(file_size))
+
+    with open(file_name, 'rb') as file:
+        for _ in range(blocks):
+            data = file.read(BLOCK_SIZE)
+
+            if len(data) < BLOCK_SIZE:
+                data = pad(data, BLOCK_SIZE)
+
+            socket.sendto(data, (IP, PORT))
+
+
 while True:
     message = input('--> ')
-    send(message) 
+    send(message)
+
+    if message == SEND_FILE:
+        send_file('file.txt')
